9dbc298d2f1b44ec4b9b0d3fd9aeb4c8585a28dc
Karim Wael <devkarim@hotmail.com>
2023-06-01 02:59:01 +0300

    Replace 'then' with await when deleting ephemeral message

--- src/buttons/prayers.ts
+++ src/buttons/prayers.ts
@@ -6,15 +6,12 @@ const prayerButton: ButtonAction = {
   async execute(interaction, city) {
     const msg = await interaction.deferReply({ ephemeral: true });
     const timings = await getPrayerTimesByCity(city);
-    interaction
-      .editReply({
-        content: `Welcome to RED!\n\nPrayer Times in ${city} is:\n\n${parseReadableTimings(
-          timings
-        )}`,
-      })
-      .then(() => {
-        setTimeout(() => msg.delete(), 15000);
-      });
+    await interaction.editReply({
+      content: `Welcome to RED!\n\nPrayer Times in ${city} is:\n\n${parseReadableTimings(
+        timings
+      )}`,
+    });
+    setTimeout(() => msg.delete(), 15000);
   },
 };
 
