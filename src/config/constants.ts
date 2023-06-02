77bcab4561cbff82c4b086a114d1711aab190c0d
Karim Wael <devkarim@hotmail.com>
2023-06-03 02:52:38 +0300

    Fix improper activity text (includes 2 to)

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
 export const IS_PROD = process.env.NODE_ENV === 'production';
 export const PRAYERS_CHANNEL_ID = '1113581543210885294';
 export const CURRENT_STATUS =
-  'to your tearings | Get closer and I will smash you | I obey only devkarim ❤️';
+  'your tearings | Get closer and I will smash you | I obey only devkarim ❤️';
